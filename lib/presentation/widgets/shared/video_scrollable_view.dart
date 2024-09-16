d2ef0b1371242e4a51deb78f54b225bf920673de
GiovanniAndrex <giovanniandrex@gmail.com>
2024-09-15 22:01:28 -0500

    Botones de Like y Views

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -7,17 +8,26 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       scrollDirection: Axis.vertical,
       physics: const BouncingScrollPhysics(),
-      children: [
-        Container(
-          color: Colors.red,
-        ),
-        Container(
-          color: Colors.blue,
-        )
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+
+        return Stack(
+          children: [
+            //Video Player + Gradiente
+
+            //botones
+            Positioned(
+              child: VideoButtons(video: videoPost),
+              bottom: 40,
+              right: 20,
+            ),
+          ],
+        );
+      },
     );
   }
 }
