1b2666f866be5bc077f04fec0e187bf5d0520829
GiovanniAndrex <giovanniandrex@gmail.com>
2024-09-16 11:39:22 -0500

    Gradiente de Fondo - fin curso

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toktik/presentation/widgets/video/gradient_bg.dart';
 import 'package:video_player/video_player.dart';
 
 class FullscreenPlayer extends StatefulWidget {
@@ -55,7 +56,9 @@ class _FullscreenPlayerState extends State<FullscreenPlayer> {
                   VideoPlayer(controller),
 
                   //Gradiente
-
+                  VideoBackground(
+                    stops: const [0.8, 1.0],
+                  ),
                   //Texto
                   Positioned(
                     bottom: 50,
